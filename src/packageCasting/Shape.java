a534c524d9c5b66c63c12b3d390e1b9d7af80a15
Abhishek K <kabhik2898@gmail.com>
2022-09-22 08:18:34 +0530

    Added Subclass Shape

---
+++ src/packageCasting/Shape.java
@@ -0,0 +1,19 @@
+package packageCasting;
+
+public class Shape {
+	
+	private int num1 ;
+	
+	private int num2;
+	
+	public int setgetNum1(int num1) {
+		this.num1 = num1;
+		return this.num1;
+	}
+	
+	public int setgetNum2(int num2) {
+		this.num2 = num2;
+		return this.num2;
+	}
+
+}
